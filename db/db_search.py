e3986b6a620681acee7da07b6ee01e1b313196c0
Kotatsu1 <nikitakotatsu@gmail.com>
2023-05-08 19:13:03 +0300

    u

--- db/db_search.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Session
-from db.models import DbUserCart, DbItem
-
-
-
-def get_items(db: Session, user_id: int):
-    items = db.query(DbItem).join(DbUserCart).filter(DbUserCart.user_id == user_id).all()
-    return items
