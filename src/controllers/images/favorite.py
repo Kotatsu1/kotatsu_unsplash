0c6d6e456b5b7d6c16fd1f8bf13d469c8e61e318
Kotatsu1 <nikitakotatsu@gmail.com>
2023-05-21 07:21:04 +0300

    fix

--- src/controllers/images/favorite.py
+++ src/controllers/images/favorite.py
@@ -12,41 +12,40 @@ def favorite_image(request: Favorite):
 
     connection = database.get_connection()
     cursor = connection.cursor()
-    try:
-        if token and public_id is None:
-            raise HTTPException(status_code=404, detail='Token and public_id is required')
 
-        user_query = 'SELECT id FROM "User" WHERE "accessToken" = \'{}\''.format(token) 
-
-        cursor.execute(user_query)
-        user_id = cursor.fetchone()
-
-        if user_id is None:
-            raise HTTPException(status_code=404, detail='User not found')
-
-        existing_image_query = 'SELECT * FROM "favoriteImages" WHERE public_id = \'{}\' AND user_id = \'{}\''.format(public_id, user_id[0])
-
-        cursor.execute(existing_image_query)
-        existing_image = cursor.fetchone()
-
-
-        if existing_image is None:
-            add_image_query = 'INSERT INTO "favoriteImages" (public_id, user_id) VALUES (\'{}\', \'{}\')'.format(public_id, user_id[0])
-            cursor.execute(add_image_query)
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return 'Image added to favorites'
-
-        else:
-            remove_image_query = 'DELETE FROM "favoriteImages" WHERE public_id = \'{}\' AND user_id = \'{}\''.format(public_id, user_id[0])
-            cursor.execute(remove_image_query)
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return 'Image removed from favorites'
-        
-    except psycopg2.Error as e:
+    if token and public_id is None:
         cursor.close()
         connection.close()
-        raise HTTPException(status_code=500, detail='Failed to favorite image' + ' SQL DETAIL: ' + str(e))
+        raise HTTPException(status_code=404, detail='Token and public_id is required')
+
+    user_query = 'SELECT id FROM "User" WHERE "accessToken" = \'{}\''.format(token) 
+
+    cursor.execute(user_query)
+    user_id = cursor.fetchone()
+
+    if user_id is None:
+        cursor.close()
+        connection.close()
+        raise HTTPException(status_code=404, detail='User not found')
+
+    existing_image_query = 'SELECT * FROM "favoriteImages" WHERE public_id = \'{}\' AND user_id = \'{}\''.format(public_id, user_id[0])
+
+    cursor.execute(existing_image_query)
+    existing_image = cursor.fetchone()
+
+
+    if existing_image is None:
+        add_image_query = 'INSERT INTO "favoriteImages" (public_id, user_id) VALUES (\'{}\', \'{}\')'.format(public_id, user_id[0])
+        cursor.execute(add_image_query)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return 'Image added to favorites'
+
+    else:
+        remove_image_query = 'DELETE FROM "favoriteImages" WHERE public_id = \'{}\' AND user_id = \'{}\''.format(public_id, user_id[0])
+        cursor.execute(remove_image_query)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return 'Image removed from favorites'
