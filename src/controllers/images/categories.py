bda9b6837f2d4d61a47b0a0926099ef357f65847
Kotatsu1 <nikitakotatsu@gmail.com>
2023-06-08 00:08:13 +0300

    testing

--- src/controllers/images/categories.py
+++ src/controllers/images/categories.py
@@ -10,8 +10,6 @@ load_dotenv()
 
 config = cloudinary.config(secure=True)
 
-
-
 def get_categories() -> dict:
     response = cloudinary.api.root_folders()
     for i in response['folders']:
@@ -19,18 +17,27 @@ def get_categories() -> dict:
     return response
 
 
+def get_images_with_params(type: str, cursor: str) -> dict:
+    return cloudinary.Search().max_results("50").next_cursor(cursor).expression(f"folder:{type}").execute()
+
+
+def add_page_preview(data: dict, params: str) -> dict:
+    data.update({'page_preview': f'http://45.87.246.48:8000/page_preview/{params}.avif'})
+    return data
+
+
 def get_images_from_category(request: FetchFromCategory):
     try:
-        images = cloudinary.Search().max_results("50").next_cursor(request.next_cursor).expression(f"folder:{request.folder}").execute()
-        images.update({'page_preview': f'http://45.87.246.48:8000/page_preview/{request.folder}.avif'})
-        return images
+        images = get_images_with_params(request.folder, request.next_cursor)
+        return add_page_preview(images, request.folder)
     except Exception:
         raise HTTPException(status_code=404, detail='Cound not get images from category')
 
 
 def get_category_images_with_favorite(request: FetchCategoryFavorites):
-    all_images = cloudinary.Search().max_results("50").next_cursor(request.next_cursor).expression(f"folder:{request.category}").execute()
-    all_images.update({'page_preview': f'http://45.87.246.48:8000/page_preview/{request.category}.avif'})
+    all_images = get_images_with_params(request.category, request.next_cursor)
+    added_preview = add_page_preview(all_images, request.category)
+    resources = added_preview['resources']
     favorite_images = favorite.user_favorive_images(request.token)
 
     def mark_favorite(image):
@@ -40,5 +47,6 @@ def get_category_images_with_favorite(request: FetchCategoryFavorites):
             image['favorite'] = False
         return image
 
-    all_images['resources'] = list(map(mark_favorite, all_images['resources']))
-    return all_images
+    # resources = list(map(lambda image: image['favorite'] = True if image["public_id"] in favorite_images else image['favorite'] = False, resources))
+    resources = list(map(mark_favorite, resources))
+    return added_preview
